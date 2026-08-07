8194e9d06b72af007429674de911bf3ab69b775e
Eduardo Baumart <eduhsouza.baumt@gmail.com>
2026-08-07 16:36:15 -0300

    project done

--- main.js
+++ main.js
@@ -6,4 +6,27 @@ const btn = document.getElementById("inp-button");
 btn.addEventListener("click", () => {
   let inpText = document.getElementById("inp-text").value;
   console.log(inpText);
+  fetch(`${url}${inpText}`).then((response) =>
+    response.json().then((data) => {
+      console.log(data);
+      result.innerHTML = ` 
+       <div class="word" id="word">
+          <h3>${inpText}</h3>
+        </div>
+        <div class="details" id="details">
+          <p>${data[0].meanings[0].partOfSpeech}</p>
+          <p>${data[0].phonetics[0].text || data[0].phonetics[1].text}</p>
+        </div>
+        <p class="word-meaning" id="word-meaning">
+          ${data[0].meanings[0].definitions[0].definition}
+        </p>
+        <p class="word-example" id="word-example">
+          ${data[0].meanings[0].definitions[0].example || ""}
+        </p>`;
+    }),
+  );
 });
+
+function playSound() {
+  sound.play();
+}
